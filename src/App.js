07898dbd930117eca19a0c319a929e4bd32bd387
Jesse Sandvik <sandvikjesse@gmail.com>
2022-02-21 21:37:47 -0500

    neon social icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink } from 'react-router-dom';
+
+import './global.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul className='social-list'>
+          <li className='twitter'>
+            <NavLink to="/">
+              <i className="fa-brands fa-twitter"></i>
+            </NavLink>
+          </li>
+          <li className='instagram'>
+            <NavLink to="/recent">
+              <i className="fa-brands fa-instagram"></i>
+            </NavLink>
+          </li>
+          <li className='facebook'>
+            <NavLink to="/library">
+              <i className="fa-brands fa-facebook-f"></i>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
