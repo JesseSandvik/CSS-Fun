20dacc6c632f2e1816c46541776dfcc7dd78ce98
Jesse Sandvik <sandvikjesse@gmail.com>
2022-03-07 22:22:41 -0500

    finished hover boxes with animations

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import './global.css';
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-        <div className='box'></div>
-        <div className='box'></div>
-      </div>
     </div>
   );
 }
---
+++ src/hoverBoxes/Hoverboxes.js
@@ -0,0 +1,12 @@
+
+
+const HoverBoxes = () => {
+  return (
+    <div className='container'>
+      <div className='box'></div>
+      <div className='box'></div>
+    </div>
+  );
+}
+
+export default HoverBoxes;
