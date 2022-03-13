ca393dc5a0ffaf77c94f72817a148b46f7c6bf43
Jesse Sandvik <sandvikjesse@gmail.com>
2022-03-13 16:39:21 -0400

    sass tutorial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
-import './global.css';
+import './css/index.css';
 
 function App() {
   return (
     <section>
       <div className='box'>
-        <h3>Popular Creator Rank List</h3>
+        <h1>Creating Umbra</h1>
+        <h2>Cards</h2>
+        <div className='card'>
+          <h3 className='card-title'>This is a card title</h3>
+          <p className='card-body'>
+            This is a card body.
+            <a href='/'>Welcome to Umbra</a>
+          </p>
+        </div>
       </div>
     </section>
   );
